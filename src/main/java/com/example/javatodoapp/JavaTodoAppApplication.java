7facac8ee1c59955d3fc4a784a01609369c3209b
jihwooon <jihwooon@gmail.com>
2022-03-23 00:33:59 +0900

    First commit

---
+++ src/main/java/com/example/javatodoapp/JavaTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTodoAppApplication.class, args);
+    }
+
+}

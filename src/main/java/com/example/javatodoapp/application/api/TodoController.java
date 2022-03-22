b7fa0e153707a792ba6c40328b263421a5268ad1
jihwooon <jihwooon@gmail.com>
2022-03-23 00:48:33 +0900

    feat : Add TodoControllerTest

--- src/main/java/com/example/javatodoapp/application/api/TodoController.java
+++ src/main/java/com/example/javatodoapp/application/api/TodoController.java
@@ -1,7 +1,9 @@
 package com.example.javatodoapp.application.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/todos")
 public class TodoController {
 }
---
+++ src/test/java/com/example/javatodoapp/application/api/TodoControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.javatodoapp.application.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoControllerTest {
+
+}

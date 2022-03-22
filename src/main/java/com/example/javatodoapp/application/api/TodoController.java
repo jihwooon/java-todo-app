48fa24df9a9af08a81211fe15a4c494877161760
jihwooon <jihwooon@gmail.com>
2022-03-23 01:03:36 +0900

    feat : Add method of getTodo in TodoController

--- src/main/java/com/example/javatodoapp/application/api/TodoController.java
+++ src/main/java/com/example/javatodoapp/application/api/TodoController.java
@@ -1,9 +1,17 @@
 package com.example.javatodoapp.application.api;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
+
+    @GetMapping
+    public ResponseEntity<List<TodoResponse>> getTodo() {
+
+    }
+
 }
